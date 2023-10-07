7561c7717ff82baf8d3e5b9be18b99d4193e54c2
m3rashid <m3rashid.hussain@gmail.com>
2023-10-07 10:00:27 +0530

    updates

---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+	logo: <span>Algorithms</span>,
+	project: {
+		link: 'https://github.com/m3rashid/algorithms'
+	},
+	useNextSeoProps: () => ({
+		titleTemplate: '%s – Algorithms',
+	}),
+	primaryHue: 164,
+	editLink: { text: '' },
+	feedback: { labels: '', content: '' },
+}
